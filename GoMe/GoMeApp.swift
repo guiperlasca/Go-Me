d728af8005f0cbe67836fcfee72d7fc9c080df3f
FKlippel <laura.klippel@edu.pucrs.br>
2026-03-27 15:26:43 -0300

    firt commit

---
+++ GoMe/GoMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoMeApp.swift
+//  GoMe
+//
+//  Created by Aluno-08 on 27/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GoMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
